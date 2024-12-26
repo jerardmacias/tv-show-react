adcb62d522fb843bed2a952a23868a95a7b7f329
Gerardo Macias <correojerard@gmail.com>
2024-12-26 20:27:30 +0100

    demo

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=74fb5d110870354d500ee21f7f7cad37";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
